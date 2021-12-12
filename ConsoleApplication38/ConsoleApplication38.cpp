249de8098c65f7b068060001c38b1df4717a2ae7
SergeyFilyaev <s.filyaev@mail.ru>
2021-12-12 21:52:12 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication38/ConsoleApplication38.cpp
@@ -0,0 +1,41 @@
+﻿
+#include <iostream>
+#include<cmath>
+
+int main()
+{
+	double a = 0;
+	double b( 3 );
+
+	
+	double xbeg(0);
+	double xend(0);
+	double epsilon(0.001);
+
+	int counter(0);
+
+	std::cout << " Enter a\n";
+	std::cin >> a;
+
+	std::cout << " Enter b\n";
+	std::cin >> b;
+	do {
+		
+		if (counter == 0)
+		{
+			xbeg = a;
+		}
+		else
+		{
+			xbeg = xend;
+		}
+
+
+		counter++;
+		xend = pow((3 * pow(xbeg,2) - 6 * xbeg + 5), (1.0 / 3.0));
+		
+		
+	} while (abs(xend - xbeg) > epsilon);
+	std::cout << " Final result  " << xend << "  Xend is root of our equation";
+}
+
